a1ccb417138907550f98125a9913cf049bed902c
omkarkhopade <omkarkhopade23@gmail.com>
2026-08-20 23:19:33 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,12 @@
 const authController =  require("../controllers/authController.js");
 
 module.exports = async function (fastify, opts) {
-    fastify.post("/register", authController.register);
-    fastify.post("/login", authController.login);
-    fastify.post("/forgot-password", authController.forgotPassword);
-    fastify.post("/reset-password/:token", authController.resetPassword);
+    fastify.post("/register", authController.register); //register
+    fastify.post("/login", authController.login); //login
+    fastify.post("/forgot-password", authController.forgotPassword); //forgot password
+    fastify.post("/reset-password/:token", authController.resetPassword); //reset password
     fastify.post(
-      "/logout",
+      "/logout", // logout 
       { preHandler: [fastify.authenticate] },
       authController.logout
     );
