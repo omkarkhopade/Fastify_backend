c5a3dc377f85bd5f608322761416dcff90e7c015
omkarkhopade <omkarkhopade23@gmail.com>
2026-08-28 00:46:07 +0530

    Update jwt.js

--- plugins/jwt.js
+++ plugins/jwt.js
@@ -1,9 +1,11 @@
 const fp = require("fastify-plugin");
 
+
 module.exports = fp(async function (fastify, opts) {
   fastify.register(require("@fastify/jwt"), {
     secret: process.env.JWT_SECRET,
   });
+
 
   fastify.decorate("authenticate", async function (request, reply) {
     try {
