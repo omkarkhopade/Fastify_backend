06150a9c3a00ce7abe650adb8be86296ea058555
omkarkhopade <omkarkhopade23@gmail.com>
2026-09-02 23:57:47 +0530

    Update models

--- models/thumbnail.js
+++ models/thumbnail.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 
 const thumbnailSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+
+  user: { type: mongoose.Schema.Types.ObjectId , ref: "User", required: true },
   videoName: { type: String, required: true },
   version: { type: String },
   image: { type: String, required: true },
   paid: { type: String, default: false },
+
 });
 
 module.exports = mongoose.model("Thumbnail", thumbnailSchema);
